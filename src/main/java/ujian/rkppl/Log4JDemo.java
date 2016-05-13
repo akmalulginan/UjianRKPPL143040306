190137507676ed235c5b30cfc01777b79009659e
SB 604-14 <SB 604-14@SB604-14-PC>
2016-05-13 17:00:32 +0700

    first commit

--- src/main/java/ujian/rkppl/Log4JDemo.java
+++ src/main/java/ujian/rkppl/Log4JDemo.java
@@ -2,34 +2,31 @@ package ujian.rkppl;
 
 import org.apache.log4j.Logger;
 
-public class Log4JDemo{
-	
-	final static Logger logger = Logger.getLogger(Log4JDemo.class);
-	
-	public static void main(String[] args) {
-	
-		Log4JDemo obj = new Log4JDemo();
-		obj.runMe("Contoh Log4J");
-		
-	}
-	
-	private void runMe(String parameter){
-		
-		
-		if(logger.isDebugEnabled()){
-			logger.debug("Ini adalah debug Message : " + parameter);
-		}
-		
-		
-		if(logger.isInfoEnabled()){
-			logger.info("Ini adalah info Message : " + parameter);
-		}
-		
-		
-		logger.warn("Ini adalah warn Message : " + parameter);
-		logger.error("Ini adalah error Message : " + parameter);
-		logger.fatal("Ini adalah fatal Message : " + parameter);
-		
-	}
-	
+public class Log4JDemo {
+
+    final static Logger logger = Logger.getLogger(Log4JDemo.class);
+
+    
+    public void Logging(String parameter) {
+
+        logger.debug("Ini adalah debug Message : " + parameter);
+        logger.info("Ini adalah info Message : " + parameter);
+        logger.warn("Ini adalah warn Message : " + parameter);
+        logger.error("Ini adalah error Message : " + parameter);
+        logger.fatal("Ini adalah fatal Message : " + parameter);
+
+    }
+    
+    
+    public static void main(String[] args) {
+
+        Log4JDemo obj = new Log4JDemo();
+        obj.Logging("Log4J");
+
+    }
+    
+    
+
+    
+
 }
